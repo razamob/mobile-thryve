c6a6bdca3825082230f5fb2a7af63d775a237188
Thea Celestino <therealme.theac@gmail.com>
2020-03-14 18:24:25 -0400

    register page

--- app/src/main/java/com/myappcompany/thea/mobileappthryve/LoginActivity.java
+++ app/src/main/java/com/myappcompany/thea/mobileappthryve/LoginActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 public class LoginActivity extends AppCompatActivity {
 
     private Button loginButton;
+    private Button signupButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +24,23 @@ public class LoginActivity extends AppCompatActivity {
                 openMainActivity();
             }
         });
+
+        signupButton = (Button) findViewById(R.id.button_signup);
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSignUpActivity();
+            }
+        });
     }
 
     public void openMainActivity() {
         Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void openSignUpActivity() {
+        Intent intent = new Intent(this, SignUpActivity.class);
         startActivity(intent);
     }
 }
